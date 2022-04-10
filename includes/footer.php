33aef87800256e3fdd5cc85a262eb726b5b383b6
AayushMaheshwari <chughdiksha22@gmail.com>
2022-04-10 23:27:44 +0530

    ADDING PROJECT

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Blood bank Management System</p>
+        </div>
+        <!-- /.container -->
+    </footer>
